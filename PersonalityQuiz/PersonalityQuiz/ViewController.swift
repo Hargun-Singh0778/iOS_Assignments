1a6c359221e6d28f0b861d2ca86d0524721e224a
Hargun-Singh0778 <156606062+Hargun-Singh0778@users.noreply.github.com>
2025-07-22 19:25:39 +0530

    Personalitiy, tabBAr, OrderOfEvents

---
+++ PersonalityQuiz/PersonalityQuiz/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Hargun Singh on 22/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToQuizIntroduction(segue: UIStoryboardSegue){
+        
+    }
+    
+
+
+}
+
---
+++ TabBarController/TabBarController/YellowViewController.swift
@@ -0,0 +1,33 @@
+//
+//  YellowViewController.swift
+//  TabBarController
+//
+//  Created by Hargun Singh on 21/07/25.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBarItem = UITabBarItem(
+               title: "Home",
+               image: UIImage(systemName: "house"),
+               selectedImage: UIImage(systemName: "house.fill")
+           )
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
