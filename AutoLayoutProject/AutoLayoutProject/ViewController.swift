1e4408cdf802ea23752c6395b070d080eea0a5ed
Hargun-Singh0778 <156606062+Hargun-Singh0778@users.noreply.github.com>
2025-07-22 09:23:19 +0530

    Small Project

---
+++ AutoLayoutProject/AutoLayoutProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutProject
+//
+//  Created by Hargun Singh on 18/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AutoLayoutProject/AutoLayoutProjectTests/AutoLayoutProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  AutoLayoutProjectTests.swift
+//  AutoLayoutProjectTests
+//
+//  Created by Hargun Singh on 18/07/25.
+//
+
+import Testing
+@testable import AutoLayoutProject
+
+struct AutoLayoutProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
